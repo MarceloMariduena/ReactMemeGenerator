c257b3f142d611077fd677f2c29d14019ed46d7d
unknown <marcelomariduena@protonmail.com>
2019-05-27 15:08:39 -0400

    Updates

---
+++ precache-manifest.c13c48f32b25a06046a67eadf20a92fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fd77a4933c188c4ab03c62d7d76f18",
+    "url": "/ReactMemeGenerator/index.html"
+  },
+  {
+    "revision": "f61f79a57fcdc0f36b72",
+    "url": "/ReactMemeGenerator/static/css/main.6aacea80.chunk.css"
+  },
+  {
+    "revision": "5d3365df44baf9a8f553",
+    "url": "/ReactMemeGenerator/static/js/2.09dbe6a1.chunk.js"
+  },
+  {
+    "revision": "f61f79a57fcdc0f36b72",
+    "url": "/ReactMemeGenerator/static/js/main.996caf14.chunk.js"
+  },
+  {
+    "revision": "afc6112049d07d06a934",
+    "url": "/ReactMemeGenerator/static/js/runtime~main.a824dc95.js"
+  },
+  {
+    "revision": "c81ac51c21863b1c9251159b1ee59342",
+    "url": "/ReactMemeGenerator/static/media/trollFace.c81ac51c.png"
+  }
+]);
